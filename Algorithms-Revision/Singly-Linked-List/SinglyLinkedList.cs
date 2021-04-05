fafac0f18d35fff1f952c678c1ddd501bb0db63f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-05 17:13:14 -0400

    created node and singlylinkedlist classes

---
+++ Algorithms-Revision/Singly-Linked-List/SinglyLinkedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Revision.Singly_Linked_List
+{
+    public class SinglyLinkedList
+    {
+        private Node first;
+
+    }
+}
