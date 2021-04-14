61980951a733f88e81dbeaea63c5813983e7000e
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-14 18:15:53 -0400

    created array list

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,7 +10,12 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            
+
+            string[] topTenMovies2021 = {  "Supernova", "The Dig", "Night of the Kings" };
+
+            LinkedList<string> topMovies = new LinkedList<string>(topTenMovies2021);
+
+            topMovies.AddFirst("The Vigil");
         }
     }
 }
