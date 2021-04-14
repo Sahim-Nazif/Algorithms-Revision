4671adb883577c24dcded0ef2480852e25d6de85
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-14 19:16:55 -0400

    added a stack class with ctor

---
+++ Stack/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private int maxSize;
+        private string[] stackArray;
+        private int top;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+
+            stackArray = new string[maxSize];
+        }
+    }
+}
