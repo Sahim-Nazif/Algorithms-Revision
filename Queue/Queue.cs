c850617c33d82bcdcd28198db7da62231954b58b
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-20 22:57:26 -0400

    inser items to queue

--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -14,5 +14,41 @@ namespace Queue
         private int rear;
         private int items;
 
+        public Queue(int size)
+        {
+            maxSize = size;
+            myQueue = new long[size];
+            front = 0;
+            rear = -1;
+            items = 0;
+        }
+
+        public void insert(long j)
+        {
+
+            if (isFull())
+            {
+                Console.WriteLine("Queue is Full");
+
+            }
+            else
+            {
+                if (rear==maxSize - 1)
+                {
+                    rear = -1;
+                }
+                rear++;
+                myQueue[rear] = j;
+                items++;
+
+            }
+        }
+
+        public bool isFull()
+        {
+
+            return (items == maxSize);
+        }
+
     }
 }
