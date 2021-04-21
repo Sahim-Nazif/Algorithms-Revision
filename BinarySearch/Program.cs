e69d7faca3411186672b257976ce1d70b749198d
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-04-21 11:41:44 -0400

    called the binarySearch method in main

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -17,6 +17,15 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            int theValue = 43;
+            int[] array = new int[] { 11, 22, 43, 54, 57, 59, 62, 78 };
+
+            Console.WriteLine("Our array contains: ");
+            Array.ForEach(array, x => Console.Write(x + " "));
+            Console.Write($"\n\n The result of a binary search for {theValue} is: ");
+            Console.WriteLine(binarySearch(array, theValue));
+            Console.ReadLine();
+
         }
         /// <summary>
         /// a=array
@@ -32,6 +41,20 @@ namespace BinarySearch
             int p= 0;//beginning of the range
             int r = a.Length - 1; //end of the range aka last slot
 
+            //search for the value
+            while (p <= r)
+            {
+                int q = (p + r) / 2;//find the midpoint
+                if (x < a[q])
+
+                    r = q - 1;//set r to midpoint. we narrowed to 1st
+                              //half of the array in the case x is less
+                              //the data in slot q
+                else if (x > a[q])
+                    p = q + 1;
+                else return q;
+
+            }
 
             //if didn't value will return nothing found
             return -1;
